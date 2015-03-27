bcaa506dfc0e403a1e8cc5e8a6604d27b800b849
daniel <dantov87@hotmail.com>
2015-03-27 21:43:18 +0100

    TEST-3 branch testing

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         int i = 0;
         i=10;
+        System.out.println("hi am testing git");
         System.out.println("Hello World!");
     }
 }
