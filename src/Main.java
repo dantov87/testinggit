f648dac2f5df7f2692a9414a479868fbaec119b6
daniel <dantov87@hotmail.com>
2015-03-27 16:05:10 +0100

    add daniel hellow add white space

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         int i = 0;
         System.out.println("hello Daniel");
+
         System.out.println("Hello World!");
     }
 }
